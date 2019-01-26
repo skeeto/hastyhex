fec89ba59ae292226da7a1abb73950f3c9e46baf
Christopher Wellons <wellons@nullprogram.com>
2019-01-26 14:42:39 -0500

    Split getopt() back out
    I can't make up my mind!

--- hastyhex.c
+++ hastyhex.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#include "getopt.h"
 
 #define PROGRAM_NAME "hastyhex"
 
@@ -187,65 +188,6 @@ process_plain(FILE *in, FILE *out)
             break;
         offset += 16;
     } while (n == 16);
-}
-
-static int optind = 1;
-static int opterr = 1;
-static int optopt;
-static char *optarg;
-
-static int
-getopt(int argc, char * const argv[], const char *optstring)
-{
-    static int optpos = 1;
-    const char *arg;
-    (void)argc;
-
-    /* Reset? */
-    if (optind == 0) {
-        optind = 1;
-        optpos = 1;
-    }
-
-    arg = argv[optind];
-    if (arg && strcmp(arg, "--") == 0) {
-        optind++;
-        return -1;
-    } else if (!arg || arg[0] != '-' || !isalnum(arg[1])) {
-        return -1;
-    } else {
-        const char *opt = strchr(optstring, arg[optpos]);
-        optopt = arg[optpos];
-        if (!opt) {
-            if (opterr && *optstring != ':')
-                fprintf(stderr, "%s: illegal option: %c\n", argv[0], optopt);
-            return '?';
-        } else if (opt[1] == ':') {
-            if (arg[optpos + 1]) {
-                optarg = (char *)arg + optpos + 1;
-                optind++;
-                optpos = 1;
-                return optopt;
-            } else if (argv[optind + 1]) {
-                optarg = (char *)argv[optind + 1];
-                optind += 2;
-                optpos = 1;
-                return optopt;
-            } else {
-                if (opterr && *optstring != ':')
-                    fprintf(stderr,
-                            "%s: option requires an argument: %c\n",
-                            argv[0], optopt);
-                return *optstring == ':' ? ':' : '?';
-            }
-        } else {
-            if (!arg[++optpos]) {
-                optind++;
-                optpos = 1;
-            }
-            return optopt;
-        }
-    }
 }
 
 static void
